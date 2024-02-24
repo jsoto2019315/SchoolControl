ead24173952ad1c9ae166f77be2de52b5dd143e3
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-02-23 19:59:02 -0600

    Field student-controller has some changes

--- controllers/student-controller.js
+++ controllers/student-controller.js
@@ -2,21 +2,28 @@ const { response } = require('express');
 const bcryptjs = require('bcryptjs');
 const Student = require('../models/student');
 
+
 const studentPost = async (req, res) => {
-    const { names, email, password } = req.body;
-    const student = new Student({ names, email, password });
+    console.log('');
+    console.log('Log de studentPost.Student');
+    try {
+        const { names, email, password } = req.body;
+        const student = new Student({ names, email, password });
 
-    const salt = bcryptjs.genSaltSync();
-    student.password = bcryptjs.hashSync(password, salt);
+        const salt = bcryptjs.genSaltSync();
+        student.password = bcryptjs.hashSync(password, salt);
 
-    await student.save();
-    res.status(200).json({
-        student
-    });
+        await student.save();
+        res.status(200).json({
+            student
+        });
 
+    } catch (e) {
+        console.log('Mistake creating the student');
+        console.error(e) //Sirve para depurar el programa
+    }
 }
 
-
-module.exports={
+module.exports = {
     studentPost
 }
